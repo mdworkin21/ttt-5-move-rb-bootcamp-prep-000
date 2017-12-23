fcff07c9ba96bdbeb137217ccc337116c744f84b
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-23 21:03:51 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move (board, index, character = "X")
-  board[index] = character
+def move (array, index, character = "X")
+  array[index] = character
 end
